ad404e61a6d7e24be5b740a3849ce93d75d122ee
Ivan Kharitonchik <ivan.kharitonchik@gmail.com>
2019-10-30 14:08:53 +0200

    Database Seeds

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'remember_token' => bcrypt(str_random(10)),
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now(),
+            'group' => 'Administrator',
+        ]);
+    }
+}
